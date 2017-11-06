e0cad769a488d7c991d486d37b107e4dd5d732ad
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2017-11-06 19:21:10 -0200

    add support to non-tls connections

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 ;; Copyright (C) 2012-present, Polis Technology Inc. This program is free software: you can redistribute it and/or  modify it under the terms of the GNU Affero General Public License, version 3, as published by the Free Software Foundation. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+(require 'cemerick.pomegranate.aether)
+(cemerick.pomegranate.aether/register-wagon-factory!
+  "http" #(org.apache.maven.wagon.providers.http.HttpWagon.))
 
 (defproject polismath "0.1.0-SNAPSHOT"
   :source-paths ["src/"
@@ -87,4 +90,3 @@
                    :source-paths ["src" "dev"]
                    :env {:mongo-url "mongodb://mongo"}}
              :production {:env {}}})
-
